8c2d9a29a2dcc71d9a97b23c22a81b270d8c1cfc
EddyBess <eddy.bessah@utbm.fr>
2022-11-26 16:52:01 +0100

    Changement des models , ajout des skills

--- portoflioapp/views.py
+++ portoflioapp/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Project
-# Create your views here.
-def projects(request):
-    latest_projects = Project.objects.order_by('-date')[:3]
-    
-    return render(request, "portoflioapp/projects.html",{
-        "latest_projects" : latest_projects 
-        })
 
-def details(request , project_id):
-    project = Project.objects.get(pk = project_id)
-    return render(request , "portoflioapp/details.html",{
-        "project":project
-    })
+# Create your views here.
+
+
+def projects(request):
+
+    latest_projects = Project.objects.order_by("-date")
+    return render(
+        request, "portoflioapp/projects.html", {"latest_projects": latest_projects}
+    )
+
+    
+
+
+def details(request, project_id):
+
+    project = Project.objects.get(pk=project_id)
+    project_skills = project.skills.all()
+    return render(request, "portoflioapp/details.html", {"project": project,"project_skills":project_skills})
