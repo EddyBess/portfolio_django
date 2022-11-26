04623b506c6ff04ed0ca46399a5e0a11469dd39b
EddyBess <eddy.bessah@utbm.fr>
2022-11-26 22:06:46 +0100

    Suppresion de l'ancienne app

--- portoflioapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("projects/",views.projects , name="projects"),
-    path("projects/<int:project_id>/",views.details,name="details")
-
-]
