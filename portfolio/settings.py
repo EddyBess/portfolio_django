177ef6da5ab30815c9bcfe959388fb8b380b19c6
EddyBess <57587025+EddyBess@users.noreply.github.com>
2022-12-26 14:55:10 +0100

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ MEDIA_URL = "/images/"
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!f@0xzoqhvw8-dov-12c4m5jh5&1==@s_r+rh^fhb4=-w=5tp'
+SECRET_KEY = 'SECURITY_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
